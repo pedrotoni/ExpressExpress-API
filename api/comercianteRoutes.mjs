a234d86f6047bb1f88176579c4755dc000e66300
Luciano Freitas <lucianoft16@outlook.com>
2022-07-30 23:57:52 -0300

    "feat: Criação de rota e metodo post para a entidade comerciante"

--- api/comercianteRoutes.mjs
+++ api/comercianteRoutes.mjs
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import  ComercianteController from "../controllers/ComercianteController.mjs";
+import ComercianteController from "../controllers/ComercianteController.mjs";
 
 const routerComerciante = Router();
 
@@ -7,6 +7,7 @@ routerComerciante.get("/comerciante", ComercianteController.mostraComerciante);
 
 routerComerciante.get("/comerciante/:id", ComercianteController.mostraUmComerciante);
 
+routerComerciante.post("/comerciante", ComercianteController.criaComerciante);
 
 
 export default routerComerciante;
--- api/controllers/ComercianteController.mjs
+++ api/controllers/ComercianteController.mjs
@@ -1,29 +1,38 @@
 import db from "../models/index.mjs";
 
 class ComercianteController {
-  static async mostraComerciante(req, res) {
-    try {
-      const allComerciantes = await db.Comerciante.findAll();
-      return res.status(200).json(allComerciantes);
-    } catch (e) {
-      return res.status(500).json(e.message);
+    static async mostraComerciante(req, res) {
+        try {
+            const allComerciantes = await db.Comerciante.findAll();
+            return res.status(200).json(allComerciantes);
+        } catch (e) {
+            return res.status(500).json(e.message);
+        }
     }
-  }
 
-
-static async mostraUmComerciante(req, res) {
-    const { id } = req.params;
-    try {
-      const oneComerciante = await db.Comerciante.findOne({
-        where: {
-          id: Number(id),
-        },
-      });
-      return res.status(200).json(oneComerciante);
-    } catch (e) {
-      return res.status(500).json(e.message);
+    static async mostraUmComerciante(req, res) {
+        const { id } = req.params;
+        try {
+            const oneComerciante = await db.Comerciante.findOne({
+                where: {
+                    id: Number(id),
+                },
+            });
+            return res.status(200).json(oneComerciante);
+        } catch (e) {
+            return res.status(500).json(e.message);
+        }
     }
-  }
+    
+    static async criaComerciante(req, res) {
+        const newComerciante = req.body;
+        try {
+            const ComercianteCriado = await db.Comerciante.create(newComerciante);
+            return res.status(200).json(comercianteCriado);
+        } catch (e) {
+            return res.status(500).json(e.message);
+        }
+    }
 }
 
 export default ComercianteController;
