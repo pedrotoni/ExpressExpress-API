5e840b4ba16d13f6a8737c4a02ca7e488dcef4e2
Luciano Freitas <lucianoft16@outlook.com>
2022-07-30 22:58:24 -0300

    "feat: Criação da rota para a entidade comerciante"

--- api/routes/index.mjs
+++ api/routes/index.mjs
@@ -1,9 +1,11 @@
 import bodyParser from "body-parser"
 import routerCliente from "./clienteRoutes.mjs"
+import routerComerciante from "./comercianteRoutes.mjs"
 
 const routes = (app) => {
     app.use(bodyParser.json());
     app.use(routerCliente);
+    app.use(routerComerciante);
 }
 
 export default routes
