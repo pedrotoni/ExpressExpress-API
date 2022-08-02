e2b63f8d6ae8e692b2edbf40be1eca9fe96e7ca7
Pedro de Toni <pedrovdetoni@gmail.com>
2022-08-02 20:47:42 -0300

    feat: adição das rotas put e delete na entidade Envio

--- api/controllers/EnvioController.mjs
+++ api/controllers/EnvioController.mjs
@@ -1,4 +1,4 @@
-import db from "../index.mjs";
+import db from "../models/index.mjs";
 
 class EnvioController {
     static async mostraEnvio(req,res) {
@@ -12,13 +12,13 @@ class EnvioController {
     }
 
     static async mostraUmEnvio(req,res) {
-        const { idEnvio } = req.params;
+        const { id } = req.params;
         try {
             const oneEnvio = await db.Envio.findOne({where: {id: Number(id)}});
             if (oneEnvio) {
                 return res.status(200).json(oneEnvio);
             } else {
-                return res.status(404).json(`Não foi possível encontrar o envio nº${idEnvio}. Por favor revise o ID inserido.`)
+                return res.status(404).json(`Não foi possível encontrar o envio nº${id}. Por favor revise o ID inserido.`)
             }
         } catch (e) {
             return res.status(500).json(e.message)
@@ -29,8 +29,30 @@ class EnvioController {
         const newEnvio = req.body;
         
         try{
-            const envioCriado = await db.Cliente.create(newEnvio);
+            const envioCriado = await db.Envio.create(newEnvio);
             return res.status(200).json(envioCriado)
+        } catch (e) {
+            return res.status(500).json(e.message)
+        }
+    }
+
+    static async atualizaEnvio(req,res) {
+        const { id } = req.params;
+        const atualizacaoEnvio = req.body;
+        try {
+            await db.Envio.update(atualizacaoEnvio, {where: {id: Number(id)}});
+            const envioAtualizado = await db.Envio.findOne({where: {id: Number(id)}});
+            return res.status(200).json(envioAtualizado);
+        } catch (e) {
+            return res.status(500).json(e.message);
+        }
+    }
+
+    static async deletaEnvio(req, res) {
+        const { id } = req.params;
+        try {
+            await db.Envio.destroy({where: {id: Number(id)}});
+            return res.status(200).json(`Envio nº ${id} deletado com sucesso.`)
         } catch (e) {
             return res.status(500).json(e.message)
         }
--- api/routes/envioRoutes.mjs
+++ api/routes/envioRoutes.mjs
@@ -6,5 +6,7 @@ const routerEnvio = Router();
 routerEnvio.get("/envio", EnvioController.mostraEnvio);
 routerEnvio.get("/envio/:id", EnvioController.mostraUmEnvio);
 routerEnvio.post("/envio", EnvioController.criaEnvio);
+routerEnvio.put("/envio/:id", EnvioController.atualizaEnvio);
+routerEnvio.delete("/envio/:id", EnvioController.deletaEnvio);
 
 export default routerEnvio;
